602476265b18fcb46c1a63d4a3340972ebbd3eca
surieltech <grandesuriel@gmail.com>
2026-08-15 23:59:16 -0400

    fix: botando o projeto certo e consolidando as rotas spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-//components
-import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
-
+import NavBar from "./components/NavBar"
 import { Outlet } from "react-router-dom"
 import './App.css'
 
 function App() {
   return (
-    <div className='app'>
-      <div className="header">
-        <h2>Deixe sua avaliação</h2>
-        <p>Ficamos felizes pela sua compra! Avalie o produto no formulario abaixo:</p>
-      </div>
-      <div className="form-container">
-        <p>Etapas</p>
-        <form>
-          <div className="inputs-container"></div>
-          <div className="actions">
-            <button type="button">
-              <GrFormPrevious></GrFormPrevious>
-              <span>Voltar</span>
-            </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext></GrFormNext>
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className='App'>
+      <NavBar></NavBar>
+     <div className="container">
+      <Outlet/>
+     </div>
     </div>
   )
 }
